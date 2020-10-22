ddd623cb0ad21cc709bf8348cd9f7e34db4f2e56
masa <masato1998yg@icloud.com>
2020-10-22 21:58:48 +0900

    add sync wait

--- conccurrency/sync_wait_group.go
+++ conccurrency/sync_wait_group.go
@@ -1,6 +1,37 @@
 package conccurrency
 
-func SyncWaitGroup(count []uint16) error {
+import (
+	"fmt"
+	"sync"
+
+	"github.com/dmskdlghs213/golang_concurrency_pattern/client"
+	"github.com/labstack/gommon/log"
+)
+
+func SyncWaitGroup(counts []uint16) error {
+
+	var wg sync.WaitGroup
+	for i := 0; i < len(counts); i++ {
+		wg.Add(1)
+
+		fmt.Println("========start==========")
+		go func(i int) {
+			fmt.Println(i, "回目")
+
+			_, err := client.APICall()
+			if err != nil {
+				log.Error(err)
+			}
+
+			defer func() {
+				wg.Done()
+				if r := recover(); r != nil {
+					log.Error(fmt.Sprintf("Recovered from:%v", r))
+				}
+			}()
+		}(i)
+	}
+	wg.Wait()
 
 	return nil
 }
